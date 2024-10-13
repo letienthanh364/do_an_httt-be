0d339f39539e9ee2798c9789e65f219db87323cb
SpyBurner <zombiepigman1505@gmail.com>
2024-10-13 18:55:00 +0700

    product controller cleanup

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,11 +7,6 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
-  }
-
   @Get()
   findAll() {
     return this.productService.findAll();
@@ -22,13 +17,4 @@ export class ProductController {
     return this.productService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
-  }
 }
