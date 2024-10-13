e41af9bb6f8e2526b456cffa0acc00b0e4045ade
SpyBurner <zombiepigman1505@gmail.com>
2024-10-13 19:14:10 +0700

    find by SubCatID/ModelID/Active

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { get } from 'http';
 
 @Controller('product')
 export class ProductController {
@@ -17,4 +18,18 @@ export class ProductController {
     return this.productService.findOne(+id);
   }
 
+  @Get('subcat/:id')
+  findBySubCatID(@Param('id') id: string) {
+    return this.productService.findBySubCatID(+id);
+  }
+
+  @Get('model/:id')
+  findByModelID(@Param('id') id: string) {
+    return this.productService.findByModelID(+id);
+  }
+
+  @Get('active')
+  findActiveProducts() {
+    return this.productService.findActiveProducts();
+  }
 }
