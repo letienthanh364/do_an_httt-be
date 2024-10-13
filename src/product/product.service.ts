635f3c84e75badcfe7ff9118945f6ad4b49a47dc
SpyBurner <zombiepigman1505@gmail.com>
2024-10-13 19:14:10 +0700

    find by SubCatID/ModelID/Active

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Param } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, IsNull, LessThan, LessThanOrEqual, MoreThan, Or, Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { ProductInventory } from './entities/productInventory.entity';
 import { dataSource } from 'ormconfig';
@@ -13,8 +13,43 @@ export class ProductService {
     @InjectDataSource(dataSource) private dataSource: DataSource,
   ) {}
 
-  async findAll(): Promise<Product[]> {
-    return this.dataSource.manager.find(Product);
+  async findAll() { //Default sort by QUANTITY DESC
+    return await this.dataSource.manager.createQueryBuilder(Product, 'product')
+    .leftJoin(
+      (subquery) => {
+        return subquery
+        .select('productInventory.ProductID', 'ProductId')
+        .addSelect('SUM(productInventory.Quantity)', 'SumQuantity')
+        .from(ProductInventory, 'productInventory')
+        .groupBy('productInventory.ProductID')
+      },
+      'productQuantity',
+      'product.ProductID = productQuantity.ProductID'
+    )
+    .addSelect('productQuantity.SumQuantity', 'SumQuantity')
+    .orderBy('SumQuantity', 'DESC')
+    .getRawMany(); //GET RAW IN CASE THE FINAL VALUE IS NOT AN ENTITY
+  }
+
+  async findBySubCatID(@Param('id') subcatID: number){
+    return await this.dataSource.manager.getRepository(Product).find({
+      where: {ProductSubcategoryID: subcatID}
+    })
+  }
+
+  async findByModelID(@Param('id') modelID: number){
+    return await this.dataSource.manager.getRepository(Product).find({
+      where: {ProductModelID: modelID}
+    })
+  }
+
+  async findActiveProducts(){
+    return await this.dataSource.manager.getRepository(Product).find({
+      where: [
+        {SellStartDate: LessThanOrEqual(new Date())},
+        {SellEndDate: Or(MoreThan(new Date()), IsNull())}
+      ]
+    })
   }
 
   async findOne(ProductID: number): Promise<Product> {//TEST CONNECTION, REMOVE LATER
