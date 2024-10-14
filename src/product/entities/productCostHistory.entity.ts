923c331fdc057f4f1c5aff3936fd2bf9b6368e5b
teddylethal <letienthanh364@gmail.com>
2024-10-14 13:33:41 +0700

    add swagger document for the apis

--- src/product/entities/productCostHistory.entity.ts
+++ src/product/entities/productCostHistory.entity.ts
@@ -1,22 +1,45 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, PrimaryColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  PrimaryColumn,
+} from 'typeorm';
 import { Product } from './product.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('ProductCostHistory', { schema: 'Production' })
 export class ProductCostHistory {
+  @JoinColumn({ name: 'ProductID' })
+  @ManyToOne(() => Product, (product) => product.costHistories)
+  product: Product;
 
-    @JoinColumn({ name: 'ProductID' })
-    @ManyToOne(() => Product, product => product.costHistories)
-    product: Product;
+  @ApiProperty({
+    description: 'Start date when the cost is being applied',
+    example: '2011-05-30T17:00:00.000Z',
+  })
+  @PrimaryColumn({ type: 'datetime' })
+  StartDate: Date;
 
-    @PrimaryColumn({ type: 'datetime' })
-    StartDate: Date;
+  @ApiProperty({
+    description: 'End date when the cost is being applied',
+    example: '2011-05-30T17:00:00.000Z',
+  })
+  @Column({ type: 'datetime', nullable: true })
+  EndDate: Date;
 
-    @Column({ type: 'datetime', nullable: true })
-    EndDate: Date;
+  @ApiProperty({
+    description: 'The cost is being applied in the given range of time',
+    example: 12.3,
+  })
+  @Column({ type: 'decimal', precision: 19, scale: 4 })
+  StandardCost: number;
 
-    @Column({ type: 'decimal', precision: 19, scale: 4 })
-    StandardCost: number;
-
-    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
-    ModifiedDate: Date;
+  @ApiProperty({
+    description: 'The modified date',
+    example: '2011-05-30T17:00:00.000Z',
+  })
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  ModifiedDate: Date;
 }
--- src/product/entities/productListPriceHistory.entity.ts
+++ src/product/entities/productListPriceHistory.entity.ts
@@ -1,21 +1,38 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
-import { Product } from "./product.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Product } from './product.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('ProductListPriceHistory', { schema: 'Production' })
 export class ProductListPriceHistory {
-    @JoinColumn({ name: 'ProductID' })
-    @ManyToOne((type) => Product, (product) => product.productListPriceHistory)
-    product: Product;
+  @JoinColumn({ name: 'ProductID' })
+  @ManyToOne((type) => Product, (product) => product.productListPriceHistory)
+  product: Product;
 
-    @PrimaryColumn({ type: 'datetime' })
-    StartDate: Date;
+  @ApiProperty({
+    description: 'Start date when the price is being applied',
+    example: '2011-05-30T17:00:00.000Z',
+  })
+  @PrimaryColumn({ type: 'datetime' })
+  StartDate: Date;
 
-    @Column({ type: 'datetime', nullable: true })
-    EndDate: Date;
+  @ApiProperty({
+    description: 'End date when the price is being applied',
+    example: '2011-05-30T17:00:00.000Z',
+  })
+  @Column({ type: 'datetime', nullable: true })
+  EndDate: Date;
 
-    @Column({ type: 'decimal', precision: 19, scale: 4 })
-    ListPrice: number;
+  @ApiProperty({
+    description: 'The price is being applied in the given range of time',
+    example: 12.3,
+  })
+  @Column({ type: 'decimal', precision: 19, scale: 4 })
+  ListPrice: number;
 
-    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
-    ModifiedDate: Date;   
+  @ApiProperty({
+    description: 'The modified date',
+    example: '2011-05-30T17:00:00.000Z',
+  })
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  ModifiedDate: Date;
 }
